555c51c674a4ecb2d06fb4ea4bc4f48efbce3c6e
Nate182654 <natenate186@yahoo.com>
2014-06-09 10:14:28 -0500

    Update ArmourUnEquip.java

--- src/main/java/com/minegusta/GearUp/armourlistening/ArmourUnEquip.java
+++ src/main/java/com/minegusta/GearUp/armourlistening/ArmourUnEquip.java
@@ -1,4 +1,22 @@
 package com.minegusta.gearup.armourlistening;
 
 public class ArmourUnEquip {
+  
+    private Player p;
+    private ItemStack i;
+    private String armourName;
+    private ClickType clickType;
+
+    public ArmourUnEquip(PlayerInteractEvent e)
+    {
+        this.p = e.getPlayer();
+        this.i = e.getItem();
+    }
+
+    public ArmourUnEquip(InventoryClickEvent e)
+    {
+        this.p = (Player) e.getWhoClicked();
+        this.i = e.getCurrentItem();
+        this.clickType = e.getClick();
+    }
 }
